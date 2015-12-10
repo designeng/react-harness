8daee21ca49ac0ef3dad249a64a3113fbdd2e69b
designeng <denissavenok@gmail.com>
2015-12-10 18:34:50 +0300

    karma test passed; import error

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+
+module.exports = function (config) {
+  config.set({
+    browsers: [ 'PhantomJS' ],
+
+    // // karma only needs to know about the test bundle
+    files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'tests.bundle.js'
+    ],
+
+    frameworks: [ 'chai', 'mocha' ],
+
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-chai',
+      'karma-mocha',
+      'karma-sourcemap-loader',
+      'karma-webpack'
+    ],
+
+    // run the bundle through the webpack and sourcemap plugins
+    preprocessors: {
+      'tests.bundle.js': [ 'webpack', 'sourcemap' ]
+    },
+
+    reporters: [ 'dots' ],
+
+    singleRun: true,
+
+    // webpack config object
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            test: /\.jsx?$/
+          }
+        ],
+      },
+      plugins: [
+        new webpack.IgnorePlugin(/\.json$/),
+        new webpack.NoErrorsPlugin()
+      ]
+    },
+
+    // webpackServer: {
+    //   noInfo: true,
+    // }
+
+  });
+};
---
+++ test/components/component.spec.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import mocha from 'mocha';
+// import NameManager from '../../src/name-manager.jsx';
+
+// var jsdom = require('jsdom');
+
+describe('A', function () {
+    beforeEach(function () {
+        console.log("TEST beforeEach STARTED");
+        // global.document = jsdom.jsdom();
+        // global.window = document.parentWindow;
+    });
+
+    it('B', function () {
+        console.log("TEST STARTED");
+        // React.render(React.createElement(NameManager), document.body);
+        // assert.equal(document.querySelector('p').innerHTML, 'hello');
+    }); 
+});

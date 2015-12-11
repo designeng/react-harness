721b88d46a8981d1ed8f8696828f399416733b28
designeng <denissavenok@gmail.com>
2015-12-11 11:41:27 +0300

    arrow func magic fix

--- test/components/component.spec.js
+++ test/components/component.spec.js
@@ -2,14 +2,14 @@ import React from 'react';
 // import mocha from 'mocha';
 // import NameManager from '../../src/name-manager.jsx';
 
-describe('A', function () {
+describe('A',  () => {
     beforeEach( function () {
         console.log("TEST beforeEach STARTED");
         // global.document = jsdom.jsdom();
         // global.window = document.parentWindow;
     });
 
-    it('B', function () {
+    it('B',  () => {
         console.log("TEST STARTED");
         // React.render(React.createElement(NameManager), document.body);
         // assert.equal(document.querySelector('p').innerHTML, 'hello');
